f7da9b311244fdc34c4c97ac5cccd6de1f23e542
Ken Zeng <31870042+kenzeng24@users.noreply.github.com>
2021-09-08 15:02:47 -0400

    Update null.sql

--- null.sql
+++ null.sql
@@ -47,4 +47,29 @@ SELECT teacher.name, dept.name
 Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
 */ 
 
+SELECT teacher.name, COALESCE(mobile, '07986 444 2266')
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+
+/* 
+6. Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+*/ 
+
+SELECT teacher.name, COALESCE(dept.name, 'None') as dept
+ FROM teacher LEFT JOIN dept
+           ON (teacher.dept=dept.id)
+
+/* 
+7. Use COUNT to show the number of teachers and the number of mobile phones.
+*/ 
+
+/* COUNT ignores NULL valeus */ 
+SELECT COUNT(teacher.name), COUNT(mobile)
+FROM teacher 
+
+/* 
+8. Use COUNT and GROUP BY dept.name to show each department and the number of staff. Use a RIGHT JOIN to ensure that the Engineering department is listed.
+*/ 
+
+
 
